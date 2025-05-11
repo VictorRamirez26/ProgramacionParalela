4f7fd0c352ea50b512bba86f51beeab26d67e5f1
Victor Ramírez <108971947+VictorRamirez26@users.noreply.github.com>
2025-05-11 15:38:29 -0300

    Comentarios bingo.c

--- Tp5/bingo.c
+++ Tp5/bingo.c
@@ -1,3 +1,14 @@
+/*
+    Asignatura: Programación Paralela y Distribuida
+    Integrantes:
+        - Olivares, Agustín
+        - Ramirez, Victor
+        - Ruiz, Joaquin
+    Fecha: Abril de 2025
+    Comando compilación: mpicc bingo.c -o bingo
+    Comando ejecución: mpirun -np 4 ./bingo
+
+*/
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
